e2a227d8c5048fe56f723830ff85e0d03f8dba7c
= <=>
2024-01-13 02:04:14 -0600

    Add unit test for verifying completion service call

--- ChatBotUnitTests/Bots/ChatGptBotTest.cs
+++ ChatBotUnitTests/Bots/ChatGptBotTest.cs
@@ -1,4 +1,5 @@
-﻿using ChatGptConsoleBot.Bots;
+﻿using System.Linq.Expressions;
+using ChatGptConsoleBot.Bots;
 using ChatGptConsoleBot.Bots.ResponseStrategies;
 using ChatGptConsoleBot.Collections.CompletionApi;
 using ChatGptConsoleBot.Constants;
@@ -30,26 +31,30 @@ public class ChatGptBotTest
         
     }
 
-    public CompletionResponse CreateFakeResponse(string responseMessage, int choiceCount)
+    [Test]
+    public async Task Chat_CanPassCorrectPostBodyToCompletionService()
     {
-        var choices = new List<Choice>();
-        for (int i = 0; i < choiceCount; i++)
-        {
-            var choice = new Choice
-            {
-                Message = new Message
-                {
-                    Content = responseMessage
-                }
-            };
+        // Arrange
+        var messageToSend = "Test Message";
+        var responseMessage = "Chat Response Message";
+        var choiceCount = 1;
+        var completionResponse = this.CreateFakeResponse(responseMessage, choiceCount);
 
-            choices.Add(choice);
-        }
+        this.completionServiceMock
+            .Setup(c => c.Chat(It.IsAny<CompletionPostBody>()))
+            .Returns(Task.FromResult(completionResponse));
 
-        return new CompletionResponse
-        {
-            Choices = choices
-        };
+        // Act
+        await this.chatBot.Chat(messageToSend);
+
+        // Assert
+        var expectedPostBody = this.BuildExpectedPostBody(messageToSend);
+        Expression<Func<CompletionPostBody, bool>> isExpectedPostBody 
+            = (postBody) => this.IsPostBodyExpectedPostBody(postBody, expectedPostBody);
+
+        this.completionServiceMock
+            .Verify(c => c.Chat(It.Is(isExpectedPostBody)), Times.Once());
+
     }
 
     [Test]
@@ -88,5 +93,72 @@ public class ChatGptBotTest
 
         // Act & Assert
         Assert.ThrowsAsync<MissingChoicesException>(async () => await this.chatBot.Chat(messageToSend));
+    }
+
+    private CompletionResponse CreateFakeResponse(string responseMessage, int choiceCount)
+    {
+        var choices = new List<Choice>();
+        for (int i = 0; i < choiceCount; i++)
+        {
+            var choice = new Choice
+            {
+                Message = new Message
+                {
+                    Content = responseMessage
+                }
+            };
+
+            choices.Add(choice);
+        }
+
+        return new CompletionResponse
+        {
+            Choices = choices
+        };
+    }
+
+    private CompletionPostBody BuildExpectedPostBody(string messageToSend)
+    {
+        var messages = new Messages();
+        var systemContext = this.config.SystemContext;
+        foreach (var content in systemContext)
+        {
+            var systemMessage = new Message
+            {
+                Role = ChatRole.SYSTEM,
+                Content = content
+            };
+
+            messages.Add(systemMessage);
+        }
+
+        var userMessage = new Message
+        {
+            Role = ChatRole.USER,
+            Content = messageToSend
+        };
+        messages.Add(userMessage);
+
+        return new CompletionPostBody
+        {
+            Messages = messages,
+            Model = this.config.GptModel
+        };
+    }
+
+    private bool IsPostBodyExpectedPostBody(CompletionPostBody postBody, CompletionPostBody expectedPostBody)
+    {
+        var expectedMessages = expectedPostBody.Messages;
+        var messages = postBody.Messages;
+
+        for (int i = 0; i < expectedMessages.Count; i++)
+        {
+            if (messages[i] != expectedMessages[i])
+            {
+                return false;
+            }
+        }
+
+        return postBody.Model == expectedPostBody.Model;
     }
 }
